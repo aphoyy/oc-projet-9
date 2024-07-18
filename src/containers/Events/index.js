9cbafb311547b04b32412fa4456a7156761bc2a7
aphoyy <christophe.scherrer4@gmail.com>
2024-07-18 11:04:34 +0200

    Fix filter and pagination

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,19 +13,27 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  const filteredEvents = 
+    data?.events
+      .filter((e) => {
+        if (type === null || type === undefined) {
+          return true
+        }
+        if (e.type === type) {
+          return true;
+        }
+        return false;
+      })
+      .filter((event, index) => {
+        if (
+          (currentPage - 1) * PER_PAGE <= index &&
+          PER_PAGE * currentPage > index
+        ) {
+          return true;
+        }
+        return false;
+      });
+      console.log(filteredEvents)
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
